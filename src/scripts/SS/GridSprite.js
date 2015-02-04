83230bb33b5ffe19c25157a796071f96a96dbca9
Jamie Ly <jamie.ly@gmail.com>
2015-02-03 21:19:26 -0500

    Working on attraction display

---
+++ src/scripts/SS/GridSprite.js
@@ -0,0 +1,63 @@
+SS.GridSprite = function(grid, game, x, y) {
+  Phaser.Sprite.call(this, game, x, y);
+
+  var onGridCellDown = this.onGridCellDown = new Phaser.Signal();
+  this.inputEnabled = true;
+  this.input.useHandCursor = true;
+  var tileSize = 32;
+  var gridPixelDimensions = {
+    length: tileSize * (grid.length),
+    width: tileSize * (grid.width)
+  };
+  this.width = gridPixelDimensions.width;
+  this.height = gridPixelDimensions.height;
+
+  var gridGraphics = createGridGraphics();
+  this.addChild(gridGraphics);
+
+  this.events.onInputDown.add(onGridSpriteDown, this);
+
+  function pointerToGridPoint(ptr) {
+    return new Phaser.Point(
+      Math.floor(ptr.x / tileSize),
+      Math.floor(ptr.y / tileSize));
+  }
+
+  // converts pointer based coordinates with respect to the grid into
+  // grid-index based coordinates.
+  function onGridSpriteDown(sprite, ptr) {
+    var gridCell = pointerToGridPoint({
+      x: ptr.x,
+      y: ptr.y
+    });
+    // bounds checking
+    if(gridCell.x >= grid.width) return;
+    if(gridCell.y >= grid.length) return;
+
+    onGridCellDown.dispatch(gridCell);
+  }
+
+  function createGridGraphics() {
+    var gridGraphics = game.add.graphics();
+
+    // make sure the dimensions match the graphic so that input is handled
+    // appropriately.
+
+    gridGraphics.lineStyle(2, 0xFF0000, 1);
+
+    for(var x = 0; x <= grid.width; x ++) {
+      var worldX = x * tileSize;
+      gridGraphics.moveTo(worldX, 0);
+      gridGraphics.lineTo(worldX, gridPixelDimensions.length);
+    }
+    for(var le = 0; le <= grid.length; le ++) {
+      var worldLe = le * tileSize;
+      gridGraphics.moveTo(0, worldLe);
+      gridGraphics.lineTo(gridPixelDimensions.width, worldLe);
+    }
+    return gridGraphics;
+  };
+};
+
+SS.GridSprite.prototype = Object.create(Phaser.Sprite.prototype);
+SS.GridSprite.prototype.constructor = SS.Grid;
--- src/scripts/SS/States/Playing.js
+++ src/scripts/SS/States/Playing.js
@@ -3,59 +3,36 @@ SS.States.Playing = function(game) {
 
   var grid;
   var gridSprite;
-  var tileSize = 32;
+  var selectedAttraction;
+
+  function onGridCellDown(index) {
+    console.log(index);
+  }
+
+  function selectAttraction(attr) {
+    selectedAttraction = attr;
+    console.log(selectedAttraction);
+  }
 
   this.create = function() {
     grid = new SS.Grid(20, 16);
-    createGridGraphics();
-  };
 
-  function pointerToGridPoint(ptr) {
-    return new Phaser.Point(
-      Math.floor(ptr.x / tileSize),
-      Math.floor(ptr.y / tileSize));
-  }
+    gridSprite = new SS.GridSprite(grid, game, 0, 0);
+    game.add.existing(gridSprite);
+    gridSprite.onGridCellDown.add(onGridCellDown);
 
-  // converts pointer based coordinates with respect to the grid into
-  // grid-index based coordinates.
-  function onGridSpriteDown(sprite, ptr) {
-    console.log(pointerToGridPoint({
-      x: ptr.x,
-      y: ptr.y
-    }));
-  }
+    var mapping = _.object(
+      _.zip(
+        [Phaser.Keyboard.ONE, Phaser.Keyboard.TWO, Phaser.Keyboard.THREE],
+        SS.Attractions));
 
-  var createGridGraphics = this.createGridGraphics = function() {
-    console.log('createGridGraphics');
-
-    var gridGraphics = game.add.graphics();
-    var gridPixelDimensions = {
-      length: tileSize * (grid.length - 1),
-      width: tileSize * (grid.width - 1)
-    };
-
-    gridSprite = game.add.sprite(0, 0, null);
-    gridSprite.addChild(gridGraphics);
-    gridSprite.inputEnabled = true;
-    gridSprite.input.useHandCursor = true;
-    gridSprite.events.onInputDown.add(onGridSpriteDown, this);
-    // make sure the dimensions match the graphic so that input is handled
-    // appropriately.
-    gridSprite.width = gridPixelDimensions.width;
-    gridSprite.height = gridPixelDimensions.height;
-
-    gridGraphics.lineStyle(2, 0xFF0000, 1);
-
-    for(var x = 0; x < grid.width; x ++) {
-      var worldX = x * tileSize;
-      gridGraphics.moveTo(worldX, 0);
-      gridGraphics.lineTo(worldX, gridPixelDimensions.length);
-    }
-    for(var le = 0; le < grid.length; le ++) {
-      var worldLe = le * tileSize;
-      gridGraphics.moveTo(0, worldLe);
-      gridGraphics.lineTo(gridPixelDimensions.width, worldLe);
-    }
+    console.log(mapping);
+    _.each(mapping, function(attraction, keyboardCode) {
+      console.log(arguments);
+      game.input.keyboard.addKey(keyboardCode).onDown.add(function() {
+        selectAttraction(attraction);
+      });
+    });
   };
 
   this.render = function() {
